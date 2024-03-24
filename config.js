fab20cbf037df1e0c7066fbd87e6070c28b79bce
Shibimwol <103479989+Shibimwol@users.noreply.github.com>
2024-03-24 15:07:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
+global.Owner = ["+918606793646"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+918606793646"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+919526304282"];
 global.OwnerName = "SACHITH";
 global.BotName = "SACHITH";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
